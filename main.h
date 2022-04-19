e0696441dd3ba592136e604bbf0c68079cdc51de
Faith <mfaithesther@gmail.com>
2022-04-19 03:37:14 -0700

    modified main.h

--- main.h
+++ main.h
@@ -3,7 +3,12 @@
 
 #include <stdlib.h>
 #include <stdarg.h>
+
+/* Functions for task one */
 int _printf(const char *format, ...);
+int print_char(va_list l, flags_t *f);
+int print_string(va_list l, flags_t *f);
+int print_percentagesign(va_list l, flags_t *f);
 
 /* write_funcs */
 int _putchar(char c);
