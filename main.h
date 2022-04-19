7d8c9e4df3d35edf2237029f760f1ae1a0df0804
Faith <mfaithesther@gmail.com>
2022-04-19 02:52:26 -0700

    added writes func, updated README

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+#include <stdlib.h>
+#include <stdarg.h>
+int _printf(const char *format, ...);
+
+/* write_funcs */
+int _putchar(char c);
+int _puts(char *str);
+
+#endif
